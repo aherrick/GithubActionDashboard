c33198d488cb0f8b5f1a95332c5c6cd44a4372b2
Andrew Herrick <aj@andrewherrick.com>
2025-11-20 11:40:22 -0500

    Remove unused usings in GitHubService and cleanup Program.cs
    Eliminated unnecessary using directives from GitHubService.cs to improve code clarity. Also reordered and cleaned up service registrations in Program.cs for better maintainability.

--- GithubActionDashboard/Program.cs
+++ GithubActionDashboard/Program.cs
@@ -1,14 +1,14 @@
+using Blazored.LocalStorage;
 using GithubActionDashboard.Components;
 using GithubActionDashboard.Services;
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<GitHubService>();
 builder.Services.AddBlazoredLocalStorage();
 
